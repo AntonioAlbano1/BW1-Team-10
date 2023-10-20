561ae64bae25812995d2cd09210032f984300b14
AntonioAlbano1 <antonio-al@live.it>
2023-10-20 09:27:35 +0200

    finish

--- test.js
+++
@@ -1,4 +0,0 @@
-const percent = 180;
-let circularProgress = document.querySelector(".circular-progress");
-
-circularProgress.style.background = `conic-gradient(#00FFFF ${percent}deg , purple 0deg)`;
